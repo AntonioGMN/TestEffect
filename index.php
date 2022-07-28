2981394053f82762f6b279d34614f387e79acbf4
Antonio <antonio.garciamn@gmail.com>
2022-07-28 00:50:52 -0300

    feat: get and save on json bids

--- index.php
+++ index.php
@@ -1,15 +1,71 @@
 <?php
+
+$html = "https://compras.natal.rn.gov.br/paginas/licitacoes/consulta/?mod=5";
+
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://compras.natal.rn.gov.br/paginas/licitacoes/consulta/?mod=5");
+curl_setopt($ch, CURLOPT_URL, $html);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-$returno = curl_exec($ch);
-$page = utf8_decode($returno);
+$webPage = curl_exec($ch);
 curl_close ($ch);
 
-$form = explode('<div class="CSSTableGenerator">', $returno);
-$certo = utf8_decode($form[1]);
+$dom = new DOMDocument();
+$dom->loadHTML($webPage);
+$td = $dom->getElementsByTagName('td');
 
-echo utf8_encode($form[1]);
-//echo "<p>$//page</p>";
+$bids = [];
+$cont = 0;
+foreach ($td as $element) {
+  $text = $element->textContent;
+
+  switch($cont){
+    case '0': 
+      $licitacao = $text;
+      break;
+    case '1': 
+      $process = $text;
+      break;
+    case '2': 
+      $type = $text;
+      break;
+    case '3': 
+      $organ = $text;
+      break;
+    case '4': 
+      $date = $text;
+      break;
+    case '5': 
+      $goal = $text;
+      break;
+    default: echo 'errro';
+    }
+  
+  $cont++;
+  if($cont == 6){
+    array_push($bids, array(
+      "biddingNumber" =>  $licitacao,
+      "process" => $process,
+      "type" => $type,
+      "organ" => $organ,
+      "date" => $date,
+      "goal" => $goal
+    ));
+
+    $licitacao = null;
+    $process = null;
+    $type = null;
+    $organ = null;
+    $date = null;
+    $goal = null;
+
+    $cont=0;
+  }
+}
+$json = json_encode($bids, JSON_UNESCAPED_UNICODE + JSON_UNESCAPED_SLASHES + JSON_PRETTY_PRINT);
+
+echo "<pre>$json</pre>";
+
+print_r($td);
+?>
+
 
